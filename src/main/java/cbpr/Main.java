ba792951a4149d45f3f57f9fe899c1f57f5e41a5
dpranantha <dpranantha@bol.com>
2015-11-08 17:51:36 +0100

    fix gradle run

--- src/main/java/cbpr/Main.java
+++ src/main/java/cbpr/Main.java
@@ -9,20 +9,21 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+        String[] paths = args;
         if (args.length != 2) {
-            System.out.println("This application requires two parameters: "
-                    + "the name of a directory containing JPEG images, and a file name of a JPEG image.");
-            return;
+            paths = new String[2];
+            paths[1] = "images/gucci1.jpeg";
+            paths[0] = "images/search/";
         }
+        final String filePath = paths[1];
 
         ArrayList<ImageFeatureExtractor.ImageInImageDatabase> databaseIndex = SimpleSimilarityRanker
-                .rankAndIndexImagesBasedOnInputtedImage(args[1], args[0],
+                .rankAndIndexImagesBasedOnInputtedImage(paths[1], paths[0],
                     Option.some(SimpleSimilarityRanker.SimilarityFeature.SIFT),
                     Option.some(SimpleSimilarityRanker.SimilarityCalculator.ABSOLUTE_DIFF));
 
         databaseIndex.forEach(index -> {
-            System.out.println(index.fileName.concat(" has distance to ").concat(args[1]).concat(" = ").concat(Double.toString(index.distanceToSearchImage)));
+            System.out.println(index.fileName.concat(" has distance to ").concat(filePath).concat(" = ").concat(Double.toString(index.distanceToSearchImage)));
         });
     }
 }
